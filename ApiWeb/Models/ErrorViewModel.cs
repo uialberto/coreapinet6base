cea3937332ad91593f10679cc677445e864ef0f8
Alberto Baigorria <lbaigorria@farmacorp.com>
2022-07-04 23:08:41 -0400

    creacion y configuracion inicial de proyecto

---
+++ ApiWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
